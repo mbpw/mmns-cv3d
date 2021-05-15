d7c32d4f7993bfab9932b1845bd62ef5cefd157d
Mateusz <mateusz.bialek@o2.pl>
2021-05-16 01:31:40 +0200

    downsampling (only module); refactor

--- assignment1/MBialek_project1.py
+++ assignment1/MBialek_project1.py
@@ -12,12 +12,6 @@ import visualization_module
 import tools
 from registration_module import *
 
-# import numpy as np
-# import open3d as o3d
-#
-# # Function available in the visualizer
-# from tools import las_to_o3d
-
 """
 Point clouds placeholders
 """
@@ -30,26 +24,73 @@ GUI
 root = Tk()
 root.title("")
 
-e1 = Entry(root, width=50)
+e1 = Entry(root, width=80)
 e1.grid(row=0, column=0)
-# e1.insert(0, "FIRST POINT CLOUD")
 e1.insert(0, "D:/PW_mgr/Sem2/[CV3D] Computer Vision and 3D data processing/proj/data/01_las/chmura_dj.las")
 
-e2 = Entry(root, width=50)
+e2 = Entry(root, width=80)
 e2.grid(row=1, column=0)
-# e2.insert(0, "SECOND POINT CLOUD")
 e2.insert(0,
           "D:/PW_mgr/Sem2/[CV3D] Computer Vision and 3D data processing/proj/data/01_las/chmura_zdjecia_naziemne.las")
+
+"""GUI FUNCTIONS"""
 
 
 def open_cloud_btn1():
     e1.delete(0, END)
-    e1.insert(0, filedialog.askopenfilename())
+    e1.insert(0, filedialog.askopenfilename(filetypes=[("LAS point cloud", ".las")]))
 
 
 def open_cloud_btn2():
     e2.delete(0, END)
     e2.insert(0, filedialog.askopenfilename())
+
+
+def show_popup(text):
+    """
+    Create popup forcing user to load cloud(s) first
+
+    :param text: text to display
+    :type text: str
+    :return: -
+    """
+    window = Toplevel()
+    label = Label(window, text=f"You need to load {text} first!")
+    label.pack(fill='x', padx=50, pady=5)
+    button_close = Button(window, text="Close", command=window.destroy)
+    button_close.pack(fill='x')
+    return
+
+
+def preview(cloud_no):
+    if cloud_no == 'c1':
+        visualization_module.visualize_cloud(e1.get())
+    if cloud_no == 'c2':
+        visualization_module.visualize_cloud(e2.get())
+
+
+def visualize_both():
+    if cloud1 is None or cloud2 is None:
+        if cloud1 is not None:
+            visualization_module.visualize_both(cloud1, e2.get())
+        elif cloud2 is not None:
+            visualization_module.visualize_both(e1.get(), cloud2)
+        else:
+            visualization_module.visualize_both(e1.get(), e2.get())
+    else:
+        visualization_module.visualize_both(cloud1, cloud2)
+
+
+def load_cloud_memory(path, c_number):
+    global cloud1, cloud2
+    temp_cloud = tools.las_to_o3d(path)
+    if c_number == 'c1':
+        cloud1 = temp_cloud
+    elif c_number == 'c2':
+        cloud2 = temp_cloud
+    else:
+        return
+    print("Successfuly Loaded points from " + path + " to memory!")
 
 
 b1 = Button(root, text="...", command=open_cloud_btn1)
@@ -58,17 +99,8 @@ b1.grid(row=0, column=1)
 b2 = Button(root, text="...", command=open_cloud_btn2)
 b2.grid(row=1, column=1)
 
-
-def preview_1():
-    visualization_module.visualize_cloud(e1.get())
-
-
-def preview_2():
-    visualization_module.visualize_cloud(e2.get())
-
-
-b3 = Button(root, text="Preview", command=lambda: threading.Thread(target=preview_1).start())
-b4 = Button(root, text="Preview", command=lambda: threading.Thread(target=preview_2).start())
+b3 = Button(root, text="Preview", command=lambda: threading.Thread(target=preview('c1')).start())
+b4 = Button(root, text="Preview", command=lambda: threading.Thread(target=preview('c2')).start())
 b3.grid(row=0, column=2, padx=5)
 b4.grid(row=1, column=2)
 
@@ -84,38 +116,9 @@ b5.grid(row=0, column=4, rowspan=2)
 
 debug_var = BooleanVar()
 
-
-# Create popup
-def show_popup(text):
-    window = Toplevel()
-    label = Label(window, text=f"You need to load {text} first!")
-    label.pack(fill='x', padx=50, pady=5)
-    button_close = Button(window, text="Close", command=window.destroy)
-    button_close.pack(fill='x')
-    return
-
-
-def mtb_pick(method="Measurement", file=None):
-    global cloud1, cloud2, debug_var
-    if cloud1 is None or cloud2 is None:
-        show_popup("both clouds")
-        return
-    debug = debug_var.get()
-    _, oriented_c2 = manual_target_based(cloud1, cloud2, type=method, debug=debug, file=file)
-    cloud2 = oriented_c2
-    pass
-
-
-def load_cloud_memory(path, c_number):
-    global cloud1, cloud2
-    temp_cloud = tools.las_to_o3d(path)
-    if c_number == 'c1':
-        cloud1 = temp_cloud
-    elif c_number == 'c2':
-        cloud2 = temp_cloud
-    else:
-        return
-    print("Successfuly Loaded points from " + path + " to memory!")
+"""
+OUTLIERS
+"""
 
 
 def remove_noise(cloud):
@@ -138,35 +141,44 @@ def remove_noise(cloud):
         cloud2 = filtered
 
 
-lbl1 = Label(root, text="Point clouds registration: ", anchor="e")
-lbl1.grid(row=4, column=0)
+lbl2 = Label(root, text="Remove noise:", anchor="e")
+b11 = Button(root, text="Cloud 1", command=lambda: threading.Thread(target=remove_noise("c1")).start())
+b12 = Button(root, text="Cloud 2", command=lambda: threading.Thread(target=remove_noise("c2")).start())
+lbl2.grid(row=3, column=0, sticky='e')
+b11.grid(row=3, column=2)
+b12.grid(row=3, column=3)
+
+"""
+REGISTRATION
+"""
+
+
+def registration(method="Measurement", file=None):
+    global cloud1, cloud2, debug_var
+    if cloud1 is None or cloud2 is None:
+        show_popup("both clouds")
+        return
+    debug = debug_var.get()
+    _, oriented_c2 = manual_target_based(cloud1, cloud2, type=method, debug=debug, file=file)
+    cloud2 = oriented_c2
+    pass
+
+
+lbl1 = Label(root, text="Point clouds registration:", anchor="e")
+lbl1.grid(row=4, column=0, sticky='e')
 
 chk1 = Checkbutton(root, variable=debug_var, onvalue=True, offvalue=False)
 
 chk1.grid(row=4, column=1)
 
-b6 = Button(root, text="Manual", command=lambda: threading.Thread(target=mtb_pick()).start())
+b6 = Button(root, text="Manual", command=lambda: threading.Thread(target=registration()).start())
 b6.grid(row=4, column=2)
 
-b9 = Button(root, text="From file", command=lambda: threading.Thread(target=mtb_pick("File", "File")).start())
+b9 = Button(root, text="From file", command=lambda: threading.Thread(target=registration("File", "File")).start())
 b9.grid(row=4, column=3)
 
-b10 = Button(root, text="DMatching", command=lambda: threading.Thread(target=mtb_pick("ICP")).start())
+b10 = Button(root, text="DMatching", command=lambda: threading.Thread(target=registration("ICP")).start())
 b10.grid(row=4, column=4, padx=5)
-
-lbl2 = Label(root, text="Remove noise", anchor="e")
-b11 = Button(root, text="Cloud 1", command=lambda: threading.Thread(target=remove_noise("c1")).start())
-b12 = Button(root, text="Cloud 2", command=lambda: threading.Thread(target=remove_noise("c2")).start())
-lbl2.grid(row=3, column=0)
-b11.grid(row=3, column=2)
-b12.grid(row=3, column=3)
-
-
-def visualize_both():
-    if cloud1 is None or cloud2 is None:
-        visualization_module.visualize_both(e1.get(), e2.get())
-    else:
-        visualization_module.visualize_both(cloud1, cloud2)
 
 
 # Start GUI loop
--- assignment1/downsampling_module.py
+++ assignment1/downsampling_module.py
@@ -4,3 +4,19 @@ Project for Computer Vision and 3D Data Processing course
 MMNS, sem. 2, Warsaw University of Technology, 2021
 ------------------------------------------------------"""
 import open3d as o3d
+
+
+# Point cloud voxel size downsampling
+def pcd_downsampling(point_cloud, voxel_size=0.1):
+    voxel_point_cloud = point_cloud.voxel_down_sample(voxel_size=voxel_size)
+    print("Draw point cloud in the voxel structure - voxel_size %f: " % voxel_size)
+    o3d.visualization.draw_geometries([voxel_point_cloud])
+    return voxel_point_cloud
+
+
+# Point cloud uniform downsampling
+def uniform_downsample(point_cloud, recduction_every_k_points=10):
+    uniform_downsample_pcd = point_cloud.uniform_down_sample(every_k_points=recduction_every_k_points)
+    print("Draw point cloud with reduced every %i point: " % recduction_every_k_points)
+    o3d.visualization.draw_geometries([uniform_downsample_pcd])
+    return uniform_downsample_pcd
