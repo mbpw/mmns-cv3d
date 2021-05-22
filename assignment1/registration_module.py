e77303b783a75bd048d4ef039dcafb317f7338c9
Mateusz <mateusz.bialek@o2.pl>
2021-05-22 19:37:01 +0200

    working coordinates -> rotation matrix calculation

--- assignment1/registration_module.py
+++ assignment1/registration_module.py
@@ -9,7 +9,7 @@ import copy
 import numpy as np
 import open3d as o3d
 
-from tools import manual_point_picking, las_to_o3d
+from tools import manual_point_picking, las_to_o3d, calculate_transformation_from_coords
 
 
 def draw_registered_pcd(ref_point_cloud, oriented_point_cloud, transformation):
@@ -91,7 +91,7 @@ def registration_target_based(ref_point_cloud, oriented_point_cloud, type='Measu
         print('Manual target-based registration')
         print('Please measure min. 3 tie point on reference point cloud: ')
         point_ref = manual_point_picking(ref_point_cloud)
-        print('Please measure min. 3 tie point on point cloud being oriented: ')
+        print('Please measure min. 3 tie point on point cloud being orient: ')
         point_ori = manual_point_picking(oriented_point_cloud)
     elif type == 'File':
         print('Target-based registration based on the file')
@@ -103,6 +103,33 @@ def registration_target_based(ref_point_cloud, oriented_point_cloud, type='Measu
         # TODO: read from file
         point_ref = [20520, 377117, 1591738]
         point_ori = [3360897, 674034, 2525727]
+
+        for p1 in point_ref:
+            print(ref_point_cloud.points[p1])
+
+        for p2 in point_ori:
+            print(oriented_point_cloud.points[p2])
+
+        refer = np.array([[3.10661300e+05, 4.68301975e+06, 4.11299993e+01, 1.],
+                          [3.10665150e+05, 4.68311877e+06, 6.57600003e+01, 1.],
+                          [3.10806250e+05, 4.68312257e+06, 5.42600002e+01, 1.]])
+        orient = np.array([[3.10543046e+05, 4.68295420e+06, 3.71739064e+01, 1.],
+                           [3.10580646e+05, 4.68304435e+06, 6.31460972e+01, 1.],
+                           [3.10715147e+05, 4.68299755e+06, 5.10652218e+01, 1.]])
+        # orient = [[310542.718750, 4682953.000000, 39.221931, 1.],
+        #             [310619.656250, 4682944.000000, 17.698511, 1.],
+        #             [310580.406250, 4683043.000000, 63.092960, 1.],
+        #             [310722.562500, 4683028.500000, 18.011694, 1.]]
+        # refer = np.array([[310661.250000, 4683019.000000, 41.130001, 1.],
+        #                   [310736.656250, 4683037.500000, 22.050001, 1.],
+        #                   [310666.062500, 4683118.000000, 65.040001, 1.],
+        #                   [310803.031250, 4683154.500000, 20.270000, 1.]])
+        R, t = calculate_transformation_from_coords(orient, refer)
+        trans1 = R
+        trans1[:3, 3] = t[:3]
+        print(trans1)
+
+
     elif type == 'DM':
         print('Descriptor matching')
         print('Normal computation...')
@@ -153,6 +180,7 @@ def registration_target_based(ref_point_cloud, oriented_point_cloud, type='Measu
     if debug is True:
         print(trans)
     draw_registered_pcd(ref_point_cloud, oriented_point_cloud, trans)
+    draw_registered_pcd(ref_point_cloud, oriented_point_cloud, trans1)
     # statistical_analysis(ref_point_cloud, oriented_point_cloud, trans)
     if True:
         oriented_point_cloud.transform(trans)
@@ -221,6 +249,20 @@ if __name__ == '__main__':
     print('Starting app...')
     ref = las_to_o3d("../data/01_las/chmura_dj.las")
     ori = las_to_o3d("../data/01_las/chmura_zdjecia_naziemne.las")
-    # registration_target_based(ref, ori)
-    trans, info = registration_ICP(ref, ori, method='cicp')
-    ori.transform(trans)
+    trans, oriented = registration_target_based(ref, ori, "File", file="asd", debug=True)
+    # trans, info = registration_ICP(ref, ori, method='cicp')
+
+    t1 = np.array([[9.34166117e-01, -3.56813086e-01, -4.25294850e-03, 1.69134432e+06],
+                   [3.56835015e-01, 9.34038670e-01, 1.55091979e-02, 1.97940958e+05],
+                   [-1.56146641e-03, -1.60057681e-02, 9.99870680e-01, 7.54383217e+04],
+                   [0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
+    # print(t1)
+    draw_registered_pcd(ori, ref, t1)
+
+    t2 = np.array([[9.34179918e-01, -3.56776960e-01, -4.25222272e-03, 1.69132878e+06],
+                   [3.56798896e-01, 9.34052612e-01, 1.55005293e-02, 1.98092725e+05],
+                   [-1.55843199e-03, -1.59974716e-02, 9.99870818e-01, 7.54034691e+04],
+                   [0., 0., 0., 1.]])
+    draw_registered_pcd(ref, ori, t2)
+
+# ori.transform(trans)
