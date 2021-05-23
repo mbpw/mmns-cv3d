4e49e7a2e18fc8e1a0d5812ea6d6d75484677a2a
Mateusz <mateusz.bialek@o2.pl>
2021-05-23 20:03:09 +0200

    Readme update; GUI update

--- assignment1/MBialek_project1.py
+++ assignment1/MBialek_project1.py
@@ -27,35 +27,35 @@ root.title("")
 
 debug_var = BooleanVar()
 
-e1 = Entry(root, width=80)
-e1.grid(row=0, column=0)
-e1.insert(0, "D:/PW_mgr/Sem2/[CV3D] Computer Vision and 3D data processing/proj/data/01_las/chmura_dj.las")
+e1 = Entry(root, width=60)
+e1.grid(row=0, column=0, sticky='EW')
+e1.insert(0, "C:/Users/Mateusz/CV3D/data/01_las/chmura_dj.las")
 
-e2 = Entry(root, width=80)
-e2.grid(row=1, column=0)
+e2 = Entry(root, width=60)
+e2.grid(row=1, column=0, sticky='EW')
 e2.insert(0,
-          "D:/PW_mgr/Sem2/[CV3D] Computer Vision and 3D data processing/proj/data/01_las/chmura_zdjecia_naziemne.las")
+          "C:/Users/Mateusz/CV3D/data/01_las/chmura_zdjecia_naziemne.las")
 
 b1 = Button(root, text="...", command=lambda: open_cloud_btn1())
-b1.grid(row=0, column=1)
+b1.grid(row=0, column=1, padx=5, sticky='EW')
 
 b2 = Button(root, text="...", command=lambda: open_cloud_btn2())
-b2.grid(row=1, column=1)
+b2.grid(row=1, column=1, padx=5, sticky='EW')
 
 b3 = Button(root, text="Preview", command=lambda: threading.Thread(target=preview('c1')).start())
 b4 = Button(root, text="Preview", command=lambda: threading.Thread(target=preview('c2')).start())
-b3.grid(row=0, column=2, padx=5)
-b4.grid(row=1, column=2)
+b3.grid(row=0, column=2, padx=5, sticky='EW')
+b4.grid(row=1, column=2, padx=5, sticky='EW')
 
 b7 = Button(root, text="Load",
             command=lambda: threading.Thread(target=load_cloud_memory, args=[e1.get(), 'c1']).start())
 b8 = Button(root, text="Load",
             command=lambda: threading.Thread(target=load_cloud_memory, args=[e2.get(), 'c2']).start())
-b7.grid(row=0, column=3, padx=5)
-b8.grid(row=1, column=3)
+b7.grid(row=0, column=3, padx=5, sticky='EW')
+b8.grid(row=1, column=3, padx=5, sticky='EW')
 
 b5 = Button(root, text="Visualize both", command=lambda: threading.Thread(target=visualize_both()).start())
-b5.grid(row=0, column=4, rowspan=2)
+b5.grid(row=0, column=4, rowspan=2, sticky='EW')
 
 """BASIC GUI AND PCD VISUALIZATION FUNCTIONS"""
 
@@ -149,9 +149,9 @@ def remove_noise(cloud):
 lbl2 = Label(root, text="Remove noise:", anchor="e")
 b11 = Button(root, text="Cloud 1", command=lambda: threading.Thread(target=remove_noise("c1")).start())
 b12 = Button(root, text="Cloud 2", command=lambda: threading.Thread(target=remove_noise("c2")).start())
-lbl2.grid(row=3, column=0, sticky='e')
-b11.grid(row=3, column=2)
-b12.grid(row=3, column=3)
+lbl2.grid(row=3, column=0, sticky='EW')
+b11.grid(row=3, column=2, sticky='EW')
+b12.grid(row=3, column=3, sticky='EW')
 
 # Remove noise (outliers) statistical parameter
 f1 = Frame(root)
@@ -165,7 +165,7 @@ lbl5.pack(side='left')
 e4 = Entry(f1, width=3)
 e4.insert(0, "100")
 e4.pack(side='left')
-f1.grid(row=3, column=4, sticky='w')
+f1.grid(row=3, column=4, sticky='EW')
 
 """
 DOWNSAMPLING
@@ -211,7 +211,7 @@ lbl6.pack(side='right')
 e5 = Entry(f2, width=3)
 e5.insert(0, "10")
 e5.pack()
-f2.grid(row=4, column=4, sticky='w')
+f2.grid(row=4, column=4, sticky='EW')
 
 """
 REGISTRATION
@@ -239,29 +239,29 @@ def registration(algorithm="tb", method="Measurement", file=None):
 lbl1 = Label(root, text="Point clouds target-based registration:", anchor="e")
 lbl1.grid(row=5, column=0, sticky='e')
 
-chk1 = Checkbutton(root, variable=debug_var, onvalue=True, offvalue=False)
-
-chk1.grid(row=5, column=1)
+# chk1 = Checkbutton(root, variable=debug_var, onvalue=True, offvalue=False)
+b18 = Button(root, text="C", command=lambda: threading.Thread(target=registration("TB", "Correspondence")).start())
+b18.grid(row=5, column=1, padx=5, sticky='EW')
 
 b6 = Button(root, text="Manual", command=lambda: threading.Thread(target=registration("TB", "Measurement")).start())
-b6.grid(row=5, column=2)
+b6.grid(row=5, column=2, padx=5, sticky='EW')
 
 b9 = Button(root, text="From file", command=lambda: threading.Thread(target=registration("TB", "File", "File")).start())
-b9.grid(row=5, column=3)
+b9.grid(row=5, column=3, padx=5, sticky='EW')
 
 b10 = Button(root, text="DMatching", command=lambda: threading.Thread(target=registration("TB", "DM")).start())
-b10.grid(row=5, column=4, padx=5)
+b10.grid(row=5, column=4, padx=5, sticky='EW')
 
 lbl3 = Label(root, text="Point clouds ICP registration:", anchor="e")
-lbl3.grid(row=6, column=0, sticky='e')
+lbl3.grid(row=6, column=0, padx=5, sticky='EW')
 b15 = Button(root, text="Point2Point", command=lambda: threading.Thread(target=registration("ICP", "p2p")).start())
-b15.grid(row=6, column=2)
+b15.grid(row=6, column=2, padx=5, sticky='EW')
 
 b16 = Button(root, text="Point2Plane", command=lambda: threading.Thread(target=registration("ICP", "p2pl")).start())
-b16.grid(row=6, column=3)
+b16.grid(row=6, column=3, padx=5, sticky='EW')
 
 b17 = Button(root, text="ColoredICP", command=lambda: threading.Thread(target=registration("ICP", "cicp")).start())
-b17.grid(row=6, column=4, padx=5)
+b17.grid(row=6, column=4, padx=5, sticky='EW')
 
 """
 COMBINE CLOUDS AND CREATE TIN
@@ -295,11 +295,17 @@ def save_cloud():
 
 
 f3 = Frame(root)
-b15 = Button(f3, text="Generate Mesh", command=lambda: threading.Thread(target=generate_mesh()).start)
-b16 = Button(f3, text="Save combined Cloud", command=lambda: threading.Thread(target=save_cloud()).start)
-f3.grid(row=7, column=0, sticky="nsew")
+b15 = Button(f3, text="Generate mesh", command=lambda: threading.Thread(target=generate_mesh()).start)
+b16 = Button(f3, text="Save combined cloud", command=lambda: threading.Thread(target=save_cloud()).start)
+f3.grid(row=7, column=1, pady=10, padx=10, columnspan=4, sticky="nsew")
 b15.pack(side="left")
 b16.pack(side="right")
+
+for x in range(5):
+    Grid.columnconfigure(root, x, weight=1)
+
+for y in range(7):
+    Grid.rowconfigure(root, y, weight=1)
 
 # Start GUI loop
 mainloop()
--- assignment1/README.md
+++ assignment1/README.md
@@ -27,44 +27,80 @@ The required program was supposed to perform Point Cloud processing including fo
 
 # Usage
 
-Run MBialek_project1.py
+### Starting the program
+
+Firstly, run the *MBialek_project1.py* file.
 
 You will see the program's main window which is GUI created in tkinter to simplify the usage of the functions by the
 user.
 
+![Program's main window graphic interface](./GUI_readme.jpg)
+
+### Loading and visualizing
+
 Firstly you need to (1) ***load the point clouds*** using the **[Load]** button. The first row is resembling the
-reference point clouds, the second one is the one we want to orient (register). You can navigate to the .las file
+reference point clouds, the second one is the one we want to orient (register). You can navigate to the _.las_ file
 via **[...]** button and next preview the chosen file by clicking **[Preview]**. It will not overwrite the current state
-of clouds that are already loaded - just visualize the selected .las file from the disk.
+of clouds that are already loaded - just visualize the selected _.las_ file from the disk.
 
 After choosing the right files and loading them into memory you can display both of them using **[Visualize both]**
 button.
 
-To use the (2) **_filtration function_** enter the desired value in the next row (Remove noise) "std:" **input field**
+### Filtering
+
+To use the (2) ***filtration function*** enter the desired value in the next row (Remove noise) "std:" **input field**
 and then press **[Cloud 1]** or **[Cloud 2]** depending on the cloud you'd like to filter. It will perform noise
 filtration using statistical method - for each point the average distance betwen the point and n of nearest neighboursis
 calculated and then the points that are further than mean+/-std. parameter multiplied by standard deviation value are
 filtrated. The default proposed values are n=100, std. (multiplier)=1.5.
 
+### Downsampling
+
 The next possibility is to use (3) ***point cloud downsampling*** which is loading only part of the cloud. In the case
 of this program I decided to implement it as a uniform downsampling, which means every k-th point is loaded from each
 cloud. The parameter k is changeable in the GUI.
 
+### Registration
+
 The main part of the program is (4) ***performing point clouds registration*** i.e. transforming one of them (oriented,
 cloud 2), so it matches the first one (reference, cloud 1). There are numbers of methods to do this that have been
 implemented in this project. Each algorithm returns the information about the resultant *fitness* (the bigger the
-better, range between 0 and 1) and *RMSE* (the lower the better).
+better, range between 0 and 1) and *RMSE* (the lower the better). Following methods are available:
+
+- C (Correspondence)
+
+Three points that were chosen arbitrary are used to calculate . This method is very fast, used for testing and works
+ONLY for unchanged point clouds (without using downsample or outlier removal).
+
+- Manual
+
+The user picks min. 3 points on first (reference) point cloud using SHIFT+left mouse button, closes the window and then
+the same points on the second one (being oriented). After closing the second window the transformation matrix is
+calculated and transformed cloud is displayed.
+
+- File
+
+With spaces or tabs between them. Example of the file:
+> 310661.30	4683019.75	41.13	310543.05	4682954.20	37.17 310665.15	4683118.77	65.76	310580.65	4683044.35	63.15 310806.25	4683122.57	54.26	310715.15	4682997.55	51.07
+
+- DMatching
+
+### Fine (ICP) registration
 
 The fine registration can be performed via (5) ***ICP registration*** using one of three algorithms: PointToPoint,
 PointToPlane or ColoredICP. Similarily, we can then obtain statistics of the resultant registration - *fitness* and
 *RMSE*.
 
-Lastly, the user can (6) ***combine both clouds*** and (7) ***save the result*** as the new .las file
-using **[Save combined cloud]** button or save it as an irregular triangle mesh in .ply format using **[Generate mesh]**
+### Combining and saving
+
+Lastly, the user can (6) ***combine both clouds*** and (7) ***save the result*** as the new _.las_ file
+using **[Save combined cloud]** button or save it as an irregular triangle mesh in _.ply_ format
+using **[Generate mesh]**
 .
 
 ___
-Of course you can also use each module separately in .py scripts (without GUI). Example code is provided in
+Of course you can also use each module (visualizing, outlier removal, downsampling, registration, mesh generating)
+separately in *.py* scripts (without GUI). Example code is provided in
 ___test.py___.
 
 # License
